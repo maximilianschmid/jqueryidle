7f66ed6adec6af843dc2574fa31b4187d36dadfa
Maximilian Schmid <ms@anorak.io>
2014-02-28 18:32:09 +0100

    initial commit

---
+++ jquery.idle.js
@@ -0,0 +1,69 @@
+(function ($) {
+  $.fn.idle = function (onidle, onactive, options) {
+    return this.each(function () {
+      var isidle = false,
+        hasMoved = false,
+        lastMove = (new Date()).getTime(),
+        opts,
+        handleUserEvent;
+
+      if ($.isPlainObject(onactive)) {
+        options = onactive;
+      }
+
+      if (!$.isFunction(onactive)) {
+        onactive = $.noop;
+      }
+
+      opts = $.extend({}, $.fn.idle.defaults, options);
+
+      $this = $(this);
+
+      $this.on({
+        'touchstart': function (e) {
+          handleUserEvent(e);
+        },
+        'mousemove': function (e) {
+          handleUserEvent(e);
+        },
+        'keydown': function (e) {
+          handleUserEvent(e);
+        }
+      });
+
+      if (Reveal){
+        Reveal.addEventListener('slidechanged', function (e) {
+          handleUserEvent(e);
+        });
+      }
+
+      handleUserEvent = function (e) {
+//        console.log("userevent", e);
+        hasMoved = true;
+        lastMove = (new Date()).getTime();
+        if (isidle) {
+          onactive.call(this);
+          isidle = false;
+        }
+      }
+
+      window.setInterval(function () {
+        if ((new Date()).getTime() - lastMove > opts.after) {
+          if (hasMoved) {
+            onidle.call(this);
+          }
+          lastMove = (new Date()).getTime();
+          isidle = true;
+        }
+      }, opts.interval);
+    });
+  };
+
+  // Set outside so they can be overrided
+  // globally before being called on
+  // an item
+  $.fn.idle.defaults = {
+    after: 5000,
+    interval: 100
+  };
+}(jQuery));
